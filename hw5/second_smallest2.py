7a1c1b846453d963f75edc594f0dbb70c162b577
Cyril Rogozhin <rogozhin.cyril@gmail.com>
2023-02-10 10:52:54 +0200

    feedback

--- hw5/second_smallest2.py
+++ hw5/second_smallest2.py
@@ -5,3 +5,11 @@ def minTwo(x):
 a = [68,54,77,31,65,23,43,567,6765,32,45]
 minSecNumber = minTwo(a)
 print(minSecNumber)
+
+def second_smallest(array):
+    array.sort()
+    return array[1]
+
+assert second_smallest([1, 1, 2, 2, 3]) == 2#Не процде такий тест
+assert second_smallest([1, 2, 2, 3]) == 2
+assert second_smallest([-1, 10, -2, 2]) == -1
